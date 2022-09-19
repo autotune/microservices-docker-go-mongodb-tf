aa4d163a6149e30fdb45403324e38dd88eea525b
Brian Adams <service@contrasting.org>
2022-09-18 23:28:43 -0500

    feat: add kubernetes ingress gateway for cinema website

---
+++ terraform/kubernetes_allow_egress_googleapis.tf
@@ -0,0 +1,29 @@
+resource "kubernetes_manifest" "allow-egress-googleapis" {
+  provider   = kubernetes.cinema
+  depends_on = [module.gke-cinema, kubernetes_namespace.cinema]
+  manifest = {
+    "apiVersion" = "networking.istio.io/v1alpha3"
+    "kind"       = "ServiceEntry"
+    "metadata" = {
+      "name" = "allow-egress-googleapis"
+    }
+    "spec" = {
+      "hosts" = [
+        "accounts.google.com",
+        "*.googleapis.com",
+      ]
+      "ports" = [
+        {
+          "name"     = "http"
+          "number"   = 80
+          "protocol" = "HTTP"
+        },
+        {
+          "name"     = "https"
+          "number"   = 443
+          "protocol" = "HTTPS"
+        },
+      ]
+    }
+  }
+}
---
+++ terraform/kubernetes_allow_egress_googlemetadata.tf
@@ -0,0 +1,31 @@
+resource "kubernetes_manifest" "allow-egress-googleapis" {
+  provider   = kubernetes.cinema
+  depends_on = [module.gke-cinema, kubernetes_namespace.cinema]
+  manifest = {
+    "apiVersion" = "networking.istio.io/v1alpha3"
+    "kind"       = "ServiceEntry"
+    "metadata" = {
+      "name" = "allow-egress-google-metadata"
+    }
+    "spec" = {
+      "addresses" = [
+        "169.254.169.254",
+      ]
+      "hosts" = [
+        "metadata.google.internal",
+      ]
+      "ports" = [
+        {
+          "name"     = "http"
+          "number"   = 80
+          "protocol" = "HTTP"
+        },
+        {
+          "name"     = "https"
+          "number"   = 443
+          "protocol" = "HTTPS"
+        },
+      ]
+    }
+  }
+}
