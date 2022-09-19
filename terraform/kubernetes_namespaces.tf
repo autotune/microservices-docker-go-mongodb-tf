4a167d398c98732ec7ef0dad6979ca1f55059fcd
Brian Adams <service@contrasting.org>
2022-09-18 21:56:25 -0500

    fix: enable asm for app

--- terraform/kubernetes_namespaces.tf
+++ terraform/kubernetes_namespaces.tf
@@ -13,3 +13,33 @@ resource "kubernetes_namespace" "external-dns" {
     name = "dns"
   }
 }
+
+resource "kubernetes_namespace" "istio-gateway" {
+  depends_on = [module.gke-cinema]
+  provider   = kubernetes.cinema
+  metadata {
+    name = "istio-gateway"
+  }
+}
+
+resource "kubernetes_labels" "default-istio" {
+  api_version = "v1"
+  kind        = "Namespace"
+  metadata {
+    name = "istio-asm"
+  }
+  labels = {
+    "istio.io/rev" = "asm-managed-regular"
+  }
+}
+
+resource "kubernetes_annotations" "cinema" {
+  api_version = "v1"
+  kind        = "Namespace"
+  metadata {
+    name = "asm-dataplane"
+  }
+  annotations = {
+    "mesh.cloud.google.com/proxy" = '{"managed":"true"}'
+  }
+}
