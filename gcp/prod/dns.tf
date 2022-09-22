ccb9470111f536774125f508240e9f87f43e30c8
Brian Adams <service@contrasting.org>
2022-09-22 04:16:59 -0500

    feat: add do DNS

--- gcp/prod/dns.tf
+++ gcp/prod/dns.tf
@@ -3,3 +3,12 @@ resource "google_dns_managed_zone" "primary" {
   dns_name    = var.managed_zone_name
   description = "${var.user_zone_name} DNS zone"
 }
+
+resource "google_dns_record_set" "do" {
+  name         = "do.${google_dns_managed_zone.primary.dns_name}"
+  managed_zone = google_dns_managed_zone.primary.name
+  type         = "NS"
+  ttl          = 300
+
+  rrdatas = ["ns1.digitalocean.com", "ns2.digitalocean.com", "ns3.digitalocean.com"]
+}
