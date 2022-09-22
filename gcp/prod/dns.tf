8b65834a3f61e8524beeb157e771934ac3dc3952
Brian Adams <service@contrasting.org>
2022-09-22 06:02:03 -0500

    feat: add wayofthesys.com to DO

--- gcp/prod/dns.tf
+++ gcp/prod/dns.tf
@@ -4,8 +4,8 @@ resource "google_dns_managed_zone" "primary" {
   description = "${var.user_zone_name} DNS zone"
 }
 
-resource "google_dns_record_set" "do" {
-  name         = "do.${google_dns_managed_zone.primary.dns_name}"
+resource "google_dns_record_set" "ns" {
+  name         = "${google_dns_managed_zone.primary.dns_name}"
   managed_zone = google_dns_managed_zone.primary.name
   type         = "NS"
   ttl          = 300
