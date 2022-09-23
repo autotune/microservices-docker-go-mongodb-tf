915c0acf217ab319d1a4b6d38c8e323f0974cbba
ExperiencedDevOps <service@contrasting.org>
2022-09-22 21:08:46 -0500

    Update oidc.tf

--- gcp/prod/oidc.tf
+++ gcp/prod/oidc.tf
@@ -4,5 +4,5 @@ resource "google_identity_platform_oauth_idp_config" "oauth_idp_config" {
   client_id     = "argocd"
   issuer        = "https://accounts.google.com"
   enabled       = true
-  client_secret = var.argocd_oidc_secret
+  client_secret = var.argocd_oidc_client_secret
 }
