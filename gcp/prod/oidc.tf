e633e4f3bbe9fcf0a87c6ff1784946fec5f3d679
Brian Adams <service@contrasting.org>
2022-09-28 19:32:56 -0500

    fix: switch gcp project account

--- gcp/prod/oidc.tf
+++ gcp/prod/oidc.tf
@@ -1,7 +1,7 @@
 resource "google_identity_platform_oauth_idp_config" "oauth_idp_config" {
   name          = "ArgoCD"
   display_name  = "ArgoCD"
-  client_id     = "argocd"
+  client_id     = "oidc.argocd"
   issuer        = "https://accounts.google.com"
   enabled       = true
   client_secret = sensitive(var.argocd_oidc_client_secret)
