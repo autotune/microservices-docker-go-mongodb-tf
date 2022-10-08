de96004c9e4b77982bdd7e1d4dd9e579a89466bc
Brian Adams <service@contrasting.org>
2022-10-08 18:01:42 -0500

    feat: adding robusta.dev playbooks

--- digitalocean/argocd.tf
+++ digitalocean/argocd.tf
@@ -183,6 +183,16 @@ resource "argocd_application" "cinema-robusta" {
         }
 
         parameter {
+          name  = "customPlaybooks[0].on_replicaset_update"
+          value = "{}"
+        }
+
+        parameter {
+          name  = "customPlaybooks[0].actions[0].report_scheduling_succeded"
+          value = "{}"
+        }
+
+        parameter {
           name  = "runner.sendAdditionalTelemetry"
           value = "false"
         }
