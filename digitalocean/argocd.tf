6d5647dfb3328a05f8ceb37b4fb88ad086e174ff
Brian Adams <service@contrasting.org>
2022-10-08 21:33:42 -0500

    added robusta.dev pb

--- digitalocean/argocd.tf
+++ digitalocean/argocd.tf
@@ -183,8 +183,8 @@ resource "argocd_application" "cinema-robusta" {
         }
 
         parameter {
-          name  = "customPlaybooks[0].actions[0].on_replicaset_update"
-          value = "\\$${"
+          name  = "custom:Playbooks[0].actions[0].on_replicaset_update"
+          value = "\\$${{"
         }
 
         /* parameter {
