f07f312285ddf457f9bc7adba6cfb37ed3b7e746
Brian Adams <service@contrasting.org>
2022-10-09 17:55:13 -0500

    added robusta.dev pb

--- digitalocean/argocd.tf
+++ digitalocean/argocd.tf
@@ -195,7 +195,7 @@ resource "argocd_application" "cinema-robusta" {
 
         parameter {
           name  = "customPlaybooks[0].actions.resource_babysitter.fields_to_monitor"
-          value = jsonencode(["spec.replicas"])
+          value = replace(jsonencode(["spec.replicas"]), "'", "")
         }
 
         parameter {
