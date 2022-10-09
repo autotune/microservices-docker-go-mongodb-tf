62b0da502b68f6f41d02f6fe954f46d853f4da65
Brian Adams <service@contrasting.org>
2022-10-09 01:19:46 -0500

    added robusta.dev pb

--- digitalocean/locals.tf
+++ digitalocean/locals.tf
@@ -1,9 +1,10 @@
 locals {
-  istio-repo      = "https://istio-release.storage.googleapis.com/charts"
-  jetstack-repo   = "https://charts.jetstack.io"
-  bookinfo-repo   = "https://evry-ace.github.io/helm-charts"
-  argocd-repo     = "https://argoproj.github.io/argo-helm"
-  escape-brackets = replace("({})", "(",""))
+  istio-repo    = "https://istio-release.storage.googleapis.com/charts"
+  jetstack-repo = "https://charts.jetstack.io"
+  bookinfo-repo = "https://evry-ace.github.io/helm-charts"
+  argocd-repo   = "https://argoproj.github.io/argo-helm"
+  escape-brackets = replace("({})",
+  "(", "")
   # \u0024
   robusta_global_sinks_config = replace(yamlencode(
     [
