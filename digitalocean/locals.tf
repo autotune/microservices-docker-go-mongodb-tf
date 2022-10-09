b1e5655d56cf07d37800e8985956d693c8339d74
Brian Adams <service@contrasting.org>
2022-10-09 11:42:05 -0500

    added robusta.dev pb

--- digitalocean/locals.tf
+++ digitalocean/locals.tf
@@ -3,7 +3,7 @@ locals {
   jetstack-repo   = "https://charts.jetstack.io"
   bookinfo-repo   = "https://evry-ace.github.io/helm-charts"
   argocd-repo     = "https://argoproj.github.io/argo-helm"
-  escape-brackets = replace("\u0024$\u007B\u007D", "$", "")
+  escape-brackets = "{{\"{}\"}}"
 
   # \u0024
   robusta_global_sinks_config = replace(yamlencode(
