ee70a219b3c8dbef87b3989d75c955ae6928c502
Brian Adams <service@contrasting.org>
2022-10-11 12:36:01 -0500

    feat: enable autoscaling

--- digitalocean/argocd.tf
+++ digitalocean/argocd.tf
@@ -50,7 +50,7 @@ resource "argocd_project" "cinema" {
     }
 
     description  = "Cinema"
-    source_repos = ["https://github.com/autotune/microservices-docker-go-mongodb-tf", "https://kedacore.github.io/charts", "https://robusta-charts.storage.googleapis.com", "https://kubernetes-sigs.github.io/metrics-server"]
+    source_repos = ["https://github.com/autotune/microservices-docker-go-mongodb-tf", "https://kedacore.github.io/charts", "https://robusta-charts.storage.googleapis.com", "https://github.com/kubernetes-sigs/metrics-server"]
 
     destination {
       server    = "https://kubernetes.default.svc"
