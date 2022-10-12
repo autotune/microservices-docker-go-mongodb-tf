ee154684720763c5cf80aef5c1796c8683142367
Brian Adams <service@contrasting.org>
2022-10-12 18:32:53 -0500

    feat: added hpa for community-operator

--- digitalocean/argocd.tf
+++ digitalocean/argocd.tf
@@ -59,7 +59,7 @@ resource "argocd_project" "metrics-server" {
 
     destination {
       server    = digitalocean_kubernetes_cluster.cinema.endpoint
-      namespace = "cinam"
+      namespace = "cinama"
     }
   }
 }
