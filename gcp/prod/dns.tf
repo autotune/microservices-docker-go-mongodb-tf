6b57c660eac9f5282e342aca8aa286e74fa90ad1
Brian Adams <service@contrasting.org>
2022-10-13 15:40:20 -0500

    fix: update dns record set to use default ns

--- gcp/prod/dns.tf
+++ gcp/prod/dns.tf
@@ -4,9 +4,3 @@ resource "google_dns_managed_zone" "primary" {
   description = "${var.user_zone_name} DNS zone"
 }
 
-resource "google_dns_record_set" "ns" {
-  name         = "${google_dns_managed_zone.primary.dns_name}"
-  managed_zone = google_dns_managed_zone.primary.name
-  type         = "NS"
-  ttl          = 300
-}
