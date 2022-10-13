59ad430e97b90c0269070e9391f4c3b27993355c
Brian Adams <service@contrasting.org>
2022-10-13 12:23:44 -0500

    feat: add gke cluster

--- gcp/prod/variables.tf
+++ gcp/prod/variables.tf
@@ -66,7 +66,7 @@ variable "zerossl_eab_hmac_key_id" {
 variable "cinema_node_core_machine_type" {
   type        = string
   description = "Cinema node core machine type"
-  default     = "c2d-highcpu-4" 
+  default     = "e2-highcpu-4" 
 }
 
 variable "cinema_node_core_disk_size" {
