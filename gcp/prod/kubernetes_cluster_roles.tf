757acf3ffedf3193793667cca9910eb1336d12ff
Brian Adams <service@contrasting.org>
2022-10-15 15:14:22 -0500

    fix: added argocd service accounts

---
+++ gcp/prod/kubernetes_cluster_roles.tf
@@ -0,0 +1,36 @@
+resource "kubernetes_cluster_role" "argocd_manager" {
+  provider = kubernetes.cinema
+  metadata {
+    name = "argocd-manager-role"
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+
+  rule {
+    non_resource_urls = ["*"]
+    verbs             = ["*"]
+  }
+}
+
+resource "kubernetes_cluster_role" "loadtesting_manager" {
+  provider = kubernetes.loadtesting
+  metadata {
+    name = "loadtesting-manager-role"
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+
+  rule {
+    non_resource_urls = ["*"]
+    verbs             = ["*"]
+  }
+}
+
---
+++ gcp/prod/kubernetes_service_accounts.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_service_account" "argocd_manager" {
+  provider = kubernetes.cinema
+  metadata {
+    name      = "argocd-manager"
+    namespace = "kube-system"
+  }
+  secret {
+    name = kubernetes_secret.argocd_manager.metadata.0.name
+  }
+}
+
+resource "kubernetes_service_account" "loadtesting_manager" {
+  provider = kubernetes.loadtesting
+  metadata {
+    name      = "loadtesting-manager"
+    namespace = "kube-system"
+  }
+}
