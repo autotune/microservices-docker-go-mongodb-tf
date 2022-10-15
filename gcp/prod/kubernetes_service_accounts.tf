9f3d32943a754462564423f398960f1a9da474d5
Brian Adams <service@contrasting.org>
2022-10-15 16:35:10 -0500

    fix: add robusta vars to tf

--- gcp/prod/kubernetes_service_accounts.tf
+++ gcp/prod/kubernetes_service_accounts.tf
@@ -1,11 +1,11 @@
-resource "kubernetes_service_account" "argocd_manager" {
+resource "kubernetes_service_account" "argocd-manager" {
   provider = kubernetes.cinema
   metadata {
     name      = "argocd-manager"
     namespace = "kube-system"
   }
   secret {
-    name = kubernetes_secret.argocd_manager.metadata.0.name
+    name = kubernetes_secret.argocd-manager.metadata.0.name
   }
 }
 
