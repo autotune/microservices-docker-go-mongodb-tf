3f4807d41f0f172fa1f671c3350c96b22a51d768
Brian Adams <service@contrasting.org>
2022-10-14 21:41:26 -0500

    feat: added virtualservice and gateway

--- gcp/prod/kubernetes_secrets.tf
+++ gcp/prod/kubernetes_secrets.tf
@@ -44,16 +44,46 @@ resource "kubernetes_secret" "mongodb-auth" {
   type = "kubernetes.io/opaque"
 }
 
+resource "kubernetes_secret" "argocd-tls" {
+  provider   = kubernetes.cinema
+  depends_on = [digitalocean_kubernetes_cluster.cinema, helm_release.external-dns, kubernetes_namespace.cinema]
+  metadata {
+    name = "argocd-tls"
+    # "${replace(var.domain_name[0], ".", "-")}-tls"
+    namespace = "istio-system"
+  }
+  type = "tls"
+  data = {
+    "tls.crt" = tls_locally_signed_cert.cert.cert_pem
+    "tls.key" = tls_private_key.key.private_key_pem
+  }
+
+  lifecycle {
+    ignore_changes = [
+      data,
+      metadata
+    ]
+  }
+}
+
 resource "kubernetes_secret" "wayofthesys-tls" {
   provider   = kubernetes.cinema
   depends_on = [module.gke-cinema, kubernetes_namespace.cinema]
   metadata {
-    name      = "${replace(var.domain_name[0], ".", "-")}-cinema-tls"
+    name      = "${replace(var.domain_name[0], ".", "-")}-tls"
     namespace = "istio-system"
   }
 
   data = {
     "tls.crt" = tls_locally_signed_cert.cert.cert_pem
     "tls.key" = tls_private_key.key.private_key_pem
+  }
+
+  lifecycle {
+    ignore_changes = [
+      data,
+      metadata
+    ]
+
   }
 }
