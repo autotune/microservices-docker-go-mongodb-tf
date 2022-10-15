3c4574ab160a7638999e4b2a4c1875ae84c96fe6
Brian Adams <service@contrasting.org>
2022-10-15 16:46:20 -0500

    fix: add robusta vars to tf

---
+++ gcp/prod/data.tf
@@ -0,0 +1,8 @@
+data "kubernetes_secret" "argocd_manager" {
+  depends_on = [kubernetes_secret.argocd_manager]
+  provider   = kubernetes.cinema
+  metadata {
+    name      = "argocd-manager" # kubernetes_service_account.argocd_manager.default_secret_name
+    namespace = "kube-system"
+  }
+}
