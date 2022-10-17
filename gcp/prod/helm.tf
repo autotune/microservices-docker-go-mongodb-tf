01c22c9deefd73fa065360cc846b264cccae5099
Brian Adams <service@contrasting.org>
2022-10-17 13:00:31 -0500

    fix: start removing resources

--- gcp/prod/helm.tf
+++ gcp/prod/helm.tf
@@ -1,3 +1,4 @@
+/* 
 resource "helm_release" "external-dns" {
   provider   = helm.cinema
   depends_on = [module.gke-cinema, kubernetes_namespace.external-dns]
@@ -37,43 +38,6 @@ resource "helm_release" "external-dns" {
   }
 }
 
-/*
-resource "helm_release" "external-dns-loadtesting" {
-  provider   = helm.loadtesting
-  depends_on = [module.gke-loadtesting, kubernetes_namespace.external-dns-loadtesting]
-  name       = "external-dns"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "external-dns"
-  version    = "v6.9.0"
-  namespace  = "external-dns"
-  timeout    = 120
-  set {
-    name  = "provider"
-    value = "digitalocean"
-  }
-  set_sensitive {
-    name  = "digitalocean.apiToken"
-    value = var.do_token
-  }
-  set {
-    name  = "rbac.create"
-    value = "true"
-  }
-  set {
-    name  = "domainFilters"
-    value = "{${var.domain_name[0]}}"
-  }
-  set {
-    name  = "sources"
-    value = "{ingress,service,istio-gateway}"
-  }
-  set {
-    name  = "istio-ingress-gateway"
-    value = "istio-system/istio-ingressgateway"
-  }
-}
-*/
-
 resource "helm_release" "cert-manager" {
   provider   = helm.cinema
   depends_on = [kubernetes_namespace.cert-manager]
@@ -93,27 +57,6 @@ resource "helm_release" "cert-manager" {
   }
 }
 
-/*
-resource "helm_release" "cert-manager-loadtesting" {
-  provider   = helm.loadtesting
-  depends_on = [kubernetes_namespace.cert-manager-loadtesting]
-  name       = "cert-manager"
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  version    = "v1.9.1"
-  namespace  = "cert-manager"
-  timeout    = 120
-  set {
-    name  = "createCustomResource"
-    value = "true"
-  }
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-}
-*/
-
 resource "helm_release" "istio-base" {
   provider        = helm.cinema
   repository      = local.istio-repo
@@ -124,19 +67,6 @@ resource "helm_release" "istio-base" {
   namespace       = kubernetes_namespace.istio-system.metadata.0.name
   depends_on      = [kubernetes_namespace.istio-system]
 }
-
-/*
-resource "helm_release" "istio-base-loadtesting" {
-  provider        = helm.loadtesting
-  repository      = local.istio-repo
-  name            = "istio-base"
-  chart           = "base"
-  cleanup_on_fail = true
-  force_update    = true
-  namespace       = kubernetes_namespace.istio-system.metadata.0.name
-  depends_on      = [kubernetes_namespace.istio-system]
-}
-*/
 
 resource "helm_release" "istiod" {
   provider        = helm.cinema
@@ -169,38 +99,6 @@ resource "helm_release" "istiod" {
   depends_on = [helm_release.istio-base]
 }
 
-/*
-resource "helm_release" "istiod-loadtesting" {
-  provider        = helm.loadtesting
-  repository      = local.istio-repo
-  name            = "istiod"
-  chart           = "istiod"
-  cleanup_on_fail = true
-  force_update    = true
-  namespace       = kubernetes_namespace.istio-system.metadata.0.name
-  set {
-    name  = "meshConfig.accessLogFile"
-    value = "/dev/stdout"
-  }
-  set {
-    name  = "grafana.enabled"
-    value = "true"
-  }
-  set {
-    name  = "kiali.enabled"
-    value = "true"
-  }
-  set {
-    name  = "servicegraph.enabled"
-    value = "true"
-  }
-  set {
-    name  = "tracing.enabled"
-    value = "true"
-  }
-  depends_on = [helm_release.istio-base-loadtesting]
-}
-*/
 
 resource "helm_release" "istio-ingress" {
   provider        = helm.cinema
@@ -212,19 +110,6 @@ resource "helm_release" "istio-ingress" {
   namespace       = kubernetes_namespace.istio-system.metadata.0.name
   depends_on      = [helm_release.istiod]
 }
-
-/*
-resource "helm_release" "istio-ingress-loadtesting" {
-  provider        = helm.loadtesting
-  repository      = local.istio-repo
-  name            = "istio-ingressgateway"
-  chart           = "gateway"
-  cleanup_on_fail = true
-  force_update    = true
-  namespace       = kubernetes_namespace.istio-system.metadata.0.name
-  depends_on      = [helm_release.istiod-loadtesting]
-}
-*/
 
 resource "helm_release" "istio-egress" {
   provider        = helm.cinema
@@ -240,23 +125,6 @@ resource "helm_release" "istio-egress" {
   }
   depends_on = [helm_release.istiod]
 }
-
-/*
-resource "helm_release" "istio-egress-loadtesting" {
-  provider        = helm.loadtesting
-  repository      = local.istio-repo
-  name            = "istio-egressgateway"
-  chart           = "gateway"
-  cleanup_on_fail = true
-  force_update    = true
-  namespace       = kubernetes_namespace.istio-system.metadata.0.name
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
-  depends_on = [helm_release.istiod-loadtesting]
-}
-*/
 
 resource "helm_release" "argocd" {
   depends_on      = [kubernetes_namespace.argocd]
@@ -307,4 +175,4 @@ resource "helm_release" "cluster-issuer" {
     value = "gke-cloud-dns"
   }
 }
-
+*/
